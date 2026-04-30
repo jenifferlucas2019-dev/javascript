5a57d9646684a57564e02a39b35d4f877c2ec4ea
Jeniffer schurhaus Lucas <jenifferlucas2019@gmail.com>
2026-04-30 19:56:51 -0300

    30/04 javascript

---
+++ pépequeno.js
@@ -0,0 +1,12 @@
+//infos
+let preco,quantidade
+let valorTotal
+//leituras
+quantidade = Number(prompt("quantidade:"))
+preco = Number(prompt("preço:"))
+//proc
+valorTotal = quantidade * preco 
+//saidas
+ console.log("valor a receber: R$" + valorTotal.toFixed(2))
+//  console.log("valor a receber: R$" + valorTotal.toFixed(2))
+
